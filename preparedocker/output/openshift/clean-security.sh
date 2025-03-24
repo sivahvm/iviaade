e16d149b46465e8191d5b14a82137c05d7fecb46
SIVAPERUMAL Rajamani <sivaperumal@ibm.com>
2025-03-24 14:27:17 +0530

    adding file

--- preparedocker/output/openshift/clean-security.sh
+++
@@ -1,9 +0,0 @@
-echo "Deleting Service Accounts"
-oc delete serviceaccount verifyaccess-config
-oc delete serviceaccount verifyaccess-anyuid
-oc delete serviceaccount verifyaccess-nonroot
-
-echo "Deleting Security Context Constraint"
-oc delete -f isva-security-constraint.yaml
-
-echo "Done."
